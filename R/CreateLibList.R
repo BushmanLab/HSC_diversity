b8a4968c655e0ce487ce90db3fa7b73a9017faf5
AdelineDENIS <39525247+AdelineDENIS@users.noreply.github.com>
2019-12-11 16:33:21 +0100

    Delete CreateLibList.R

--- R/CreateLibList.R
+++
@@ -1,3 +0,0 @@
-write(file = 'requiredLibs', unique(unlist(sapply(list.files(pattern = '*.Rmd$|*.R$'), function(x){
- unlist(lapply(stringr::str_match_all(readLines(x), 'library\\(([^\\)]+)'), '[', 2))
-}))))
